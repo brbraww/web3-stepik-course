6ac6c77b33a7ac4033c511b328fcf72da6180ba7
brbr aww <legendary.grassman@gmail.com>
2020-09-06 15:13:00 +0300

    Updates

---
+++ precache-manifest.2b7ec75f003f1d7d9310e31c72e9c735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e625a7538e31fc1c0b3a512839570906",
+    "url": "/web3-stepik-course/index.html"
+  },
+  {
+    "revision": "702d4f3bcef29fef5388",
+    "url": "/web3-stepik-course/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "548f91ca1e915b6dec5d",
+    "url": "/web3-stepik-course/static/js/2.55d20a23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web3-stepik-course/static/js/2.55d20a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702d4f3bcef29fef5388",
+    "url": "/web3-stepik-course/static/js/main.8128a51b.chunk.js"
+  },
+  {
+    "revision": "dc9ff617e1f1870bd6e0",
+    "url": "/web3-stepik-course/static/js/runtime-main.07bc3d7b.js"
+  }
+]);
